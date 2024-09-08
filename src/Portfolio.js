757608e1827d76916998be708557ea41e22aa953
jkkim <jaekyeong930@gmail.com>
2024-09-09 00:55:39 +0900

    헤더 정렬 완료, 로고와 메뉴 배치 완료

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -16,58 +16,76 @@ function Portfolio( {  } ) {
 
   return (
     <div className='root'>
+    {/* 헤더 영역 */}
       <header className='Header'>
-        {/* 페이지 맨 위 상단으로 이동하는 사이트 이름, 로고 */}
+        {/* 사이트 로고, 클릭하면 맨 위로 이동 */}
         <div className='Header_logo'><Link to ='title' smooth={true}>JKKim's Portfolio</Link></div> 
-
-        {/* 햄버거 메뉴 버튼 */}
-        <button className='menu-toggle' onClick={toggleMenu}>
-          {isMenuOpen ? '✖' : '☰'}
-        </button>
         {/* 각 영역으로 이동하는 헤더 메뉴*/}
         <div className='Header_navigation'>
-          <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
-            <div className='Header_navigation_menu'><Link to ='profile' smooth={true} offset={-headerHeight}>Profile</Link></div>
-            <div className='Header_navigation_menu'><Link to ='skills' smooth={true} offset={-headerHeight}>Skills</Link></div>
-            <div className='Header_navigation_menu'><Link to ='projects' smooth={true} offset={-headerHeight}>Projects</Link></div>
-            <div className='Header_navigation_menu'><Link to ='career' smooth={true} offset={-headerHeight}>Career</Link></div>
+          <nav className='menu_container'>
+            {/* width = 992px 이하일 때 나타나는 메뉴 토글 */}
+            <button className='menu-toggle' onClick={toggleMenu}>
+              {isMenuOpen ? '✖' : '☰'}
+            </button>
+            {/* 각 메뉴. width = 992px 이하일 경우 접힌 상태로 표시 */}
+            <div className={`Header_navigation_menus ${isMenuOpen ? "open" : ""}`}>
+              <div className='Header_navigation_menu' style={{ transitionDelay: "0.2s"}}>
+                <Link to ='profile' smooth={true} offset={-headerHeight}>Profile</Link>
+              </div>
+              <div className='Header_navigation_menu' style={{ transitionDelay: "0.4s"}}>
+                <Link to ='skills' smooth={true} offset={-headerHeight}>Skills</Link>
+              </div>
+              <div className='Header_navigation_menu' style={{ transitionDelay: "0.6s"}}>
+                <Link to ='projects' smooth={true} offset={-headerHeight}>Projects</Link>
+              </div>
+              <div className='Header_navigation_menu' style={{ transitionDelay: "0.8s"}}>
+                <Link to ='career' smooth={true} offset={-headerHeight}>Career</Link>
+              </div>
+            </div>
           </nav>
         </div>
       </header>
+      {/* 맨 위 상단 프롤로그 영역. 이름, 한줄소개, 더보기 버튼 */}
       <div id='title' className='prolog' style={{height:'60vh'}}>
         <div className='prolog_content'>
+          {/* 이름 기재 */}
           <div className='prolog_content_title'>
             Jaegyeong Kim
           </div>
           <div className='prolog_content_line'>
             ━━━━
           </div>
+          {/* 한줄소개 */}
           <div className='prolog_content_introduce'>
             Hello, I'm Jaegyeong Kim, a developer who values UI/UX. 
           </div>
+          {/* 더보기 버튼 */}
           <div className='prolog_content_btn'>
             <Link to ='profile' smooth={true} offset={-headerHeight}>
             <button>more ▼</button>
             </Link>
-            
           </div>
         </div>
       </div>
+      {/* 메뉴를 각각 클릭할 경우 이동하는 영역들 */}
+      {/* 1. 프로필 영역 / 이름, 생년월일, 주소지, 연락처, 이메일, 학력, 국적을 기재 */}
       <article id='profile' className='article article_profile' style={{backgroundColor:'pink'}}>
         <div className='article_profile_title'>
-          Profile
         </div>
       </article>
+      {/* 2. 스킬 영역 / 사용 경험이 있는 기술을 추가한다. */}
       <article id='skills' className='article article_skills' style={{backgroundColor:'lightblue'}}>
         <div className='article_skills_title'>
           Skills
         </div>
       </article>
+      {/* 3. 프로젝트 영역 / 참가했던 프로젝트와 개요, 다룬 기술, 감상 등을 기재 */}
       <article id='projects' className='article article_projects' style={{backgroundColor:'lightyellow'}}>
         <div className='article_projects_title'>
         Projects
         </div>
       </article>
+      {/* 4. 커리어 영역 / 근무했던 회사와 기간을 기재 */}
       <article id='career' className='article article_career' style={{backgroundColor:'lavender'}}>
         <div className='article_carrer_title'>
         Career
